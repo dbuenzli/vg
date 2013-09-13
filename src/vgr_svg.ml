92fcaf8ebc3fddc6fb066d25de3379570db09bf1
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-09-13 16:09:21 +0100

    Follow same naming conventions in PDF and SVG renderer.

--- src/vgr_svg.ml
+++ src/vgr_svg.ml
@@ -7,9 +7,9 @@
 open Gg
 open Vg
 open Vgr.Private.Data
-
+       
 (* Renderer *)
-
+       
 type svg_font = string
 type svg_prim = Gradient of int | Color of string * string
 type g_state = { g_tr : M3.t } 
@@ -55,21 +55,21 @@ let uncut_bounds s =
 
 let cap_str = function 
 | `Butt -> "butt" | `Round -> "round" | `Square -> "square"
-
+  
 let join_str = function 
 | `Miter -> "miter" | `Bevel -> "bevel" | `Round -> "round"
-
+  
 let area_str = function 
 | `Aeo -> "evenodd" | `Anz -> "nonzero"
-
+  
 let w_str str k r = Vgr.Private.writes str 0 (String.length str) k r
 let w_buf s k r = 
   let clear k r = Buffer.clear s.buf; k r in
   Vgr.Private.writebuf s.buf 0 (Buffer.length s.buf) (clear k) r
-
-let badd_fmt s fmt = Printf.bprintf s.buf fmt
-let badd_str s str = Buffer.add_string s.buf str 
-let badd_esc_str s str = Vgr.Private.add_xml_data s.buf str 
+    
+let b_fmt s fmt = Printf.bprintf s.buf fmt
+let b_str s str = Buffer.add_string s.buf str 
+let b_esc_str s str = Vgr.Private.add_xml_data s.buf str 
 
 (* Warning the following uses s.buf *)
 let bget_font s font = try Hashtbl.find s.fonts font with 
@@ -77,25 +77,17 @@ let bget_font s font = try Hashtbl.find s.fonts font with
     (* N.B. it seems that specifying using style="font:..." or font="..." 
        results in broken behaviour in one or other of the browsers. *)
     let font_str = 
-      badd_str s "font-family=\""; 
-      badd_esc_str s font.Font.name; 
-      badd_fmt s "\" font-style=\"%s\" font-weight=\"%s\" font-size=\"%g\""
+      b_str s "font-family=\""; 
+      b_esc_str s font.Font.name; 
+      b_fmt s "\" font-style=\"%s\" font-weight=\"%s\" font-size=\"%g\""
         (Vgr.Private.Font.css_slant font) (Vgr.Private.Font.css_weight font)
         font.Font.size; 
       Buffer.contents s.buf
     in
     Hashtbl.add s.fonts font font_str; Buffer.clear s.buf; font_str
 
-let badd_title s = function
-| None -> () 
-| Some t -> badd_str s "<title>"; badd_esc_str s t; badd_str s "</title>"
-
-let badd_descr s = function
-| None -> () 
-| Some d -> badd_str s "<descr>"; badd_esc_str s d; badd_str s "</descr>"
-
-let badd_svg s xml_decl size =
-  badd_fmt s
+let b_svg s xml_decl size =
+  b_fmt s
      "%s\
      <svg xmlns=\"http://www.w3.org/2000/svg\" \
         xmlns:l=\"http://www.w3.org/1999/xlink\" \
@@ -110,37 +102,35 @@ let badd_svg s xml_decl size =
     (if xml_decl then "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" else "") 
     (Size2.w size) (Size2.h size) (Size2.w size) (Size2.h size)
 
-let badd_xmp s = match s.xmp with 
+let b_xmp s = match s.xmp with 
 | None -> () 
 | Some xmp ->
-    badd_fmt s "<?xpacket begin=\"\xEF\xBB\xBF\" \
+    b_fmt s "<?xpacket begin=\"\xEF\xBB\xBF\" \
                           id=\"W5M0MpCehiHzreSzNTczkc9d\"?>\
-                <x:xmpmeta xmlns:x=\"adobe:ns:meta/\">
-                %s\
-                </x:xmpmeta>
-                <?xpacket end=\"w\"?>" xmp
-
-let badd_init s size view = 
+             <x:xmpmeta xmlns:x=\"adobe:ns:meta/\">%s</x:xmpmeta>\
+             <?xpacket end=\"w\"?>" xmp
+      
+let b_init s size view = 
   let sx = Size2.w size /. Box2.w view in 
   let sy = Size2.h size /. Box2.h view in 
   let dx = -. Box2.ox view *. sx in 
   let dy = Size2.h size +. Box2.oy view *. sy in
-   (* stroke-width is 1. initially, so is P.o *)
-   (* stroke-linecap is butt initially, so is P.o *) 
-   (* stroke-linejoin is miter initially, so is P.o *)
-   (* stroke-dasharray is none initially, so is P.o *)
-   (* fill is black initially, we set it to none *)
-  badd_fmt s "<g fill=\"none\" stroke-miterlimit=\"%g\" \
+  (* stroke-width is 1. initially, so is P.o *)
+  (* stroke-linecap is butt initially, so is P.o *) 
+  (* stroke-linejoin is miter initially, so is P.o *)
+  (* stroke-dasharray is none initially, so is P.o *)
+  (* fill is black initially, we set it to none *)
+  b_fmt s "<g fill=\"none\" stroke-miterlimit=\"%g\" \
                  transform=\"matrix(%g %g %g %g %g %g)\">"
     (Vgr.Private.P.miter_limit P.o)
     sx 0. 0. (-. sy) dx dy (* map view rect -> viewport *)
 
-let badd_transform s = function 
-| Move v -> badd_fmt s "translate(%g %g)" (V2.x v) (V2.y v); M3.move2 v
-| Rot a -> badd_fmt s "rotate(%g)" (Float.deg_of_rad a); M3.rot2 a
-| Scale sv -> badd_fmt s "scale(%g %g)" (V2.x sv) (V2.y sv); M3.scale2 sv
+let b_transform s = function 
+| Move v -> b_fmt s "translate(%g %g)" (V2.x v) (V2.y v); M3.move2 v
+| Rot a -> b_fmt s "rotate(%g)" (Float.deg_of_rad a); M3.rot2 a
+| Scale sv -> b_fmt s "scale(%g %g)" (V2.x sv) (V2.y sv); M3.scale2 sv
 | Matrix m -> 
-    badd_fmt s "transform(%g %g %g %g %g %g)"
+    b_fmt s "transform(%g %g %g %g %g %g)"
       (M3.e00 m) (M3.e10 m) (M3.e01 m) (M3.e11 m) (M3.e02 m) (M3.e12 m); m
 
 let w_path s p k r = try k (Hashtbl.find s.paths p) r with
@@ -151,22 +141,22 @@ let w_path s p k r = try k (Hashtbl.find s.paths p) r with
     | seg :: p -> 
         match seg with 
         | `Sub pt -> 
-            badd_fmt s "M%g %g" (V2.x pt) (V2.y pt); 
+            b_fmt s "M%g %g" (V2.x pt) (V2.y pt); 
             w_buf s (w_data p k) r
         | `Line pt -> 
-            badd_fmt s "L%g %g" (V2.x pt) (V2.y pt);
+            b_fmt s "L%g %g" (V2.x pt) (V2.y pt);
             w_buf s (w_data p k) r
         | `Qcurve (c, pt) -> 
-            badd_fmt s "Q%g %g %g %g" (V2.x c) (V2.y c) (V2.x pt) (V2.y pt); 
+            b_fmt s "Q%g %g %g %g" (V2.x c) (V2.y c) (V2.x pt) (V2.y pt); 
             w_buf s (w_data p k) r               
         | `Ccurve (c, c', pt) ->
-            badd_fmt s "C%g %g %g %g %g %g" 
+            b_fmt s "C%g %g %g %g %g %g" 
               (V2.x c) (V2.y c) (V2.x c') (V2.y c') (V2.x pt) (V2.y pt); 
             w_buf s (w_data p k) r
         | `Earc (large, cw, a, radii, pt) -> 
             let large = if large then 1 else 0 in
             let sweep = if cw then 0 else 1 in
-            badd_fmt s "A %g %g %g %d %d %g %g"  
+            b_fmt s "A %g %g %g %d %d %g %g"  
               (V2.x radii) (V2.y radii) (Float.deg_of_rad a) large sweep 
               (V2.x pt) (V2.y pt);
             w_buf s (w_data p k) r
@@ -174,107 +164,107 @@ let w_path s p k r = try k (Hashtbl.find s.paths p) r with
             w_str "Z" (w_data p k) r
     in
     Hashtbl.add s.paths p id;
-    badd_fmt s "<defs><path id=\"i%d\" d=\"" id; 
+    b_fmt s "<defs><path id=\"i%d\" d=\"" id; 
     w_buf s (w_data (List.rev p) k) r
-
-let badd_rgb_color s c = 
+      
+let b_rgb_color s c = 
   let srgb = Color.to_srgb c in
   let r = Float.int_of_round (Color.r srgb *. 255.) in 
   let g = Float.int_of_round (Color.g srgb *. 255.) in 
   let b = Float.int_of_round (Color.b srgb *. 255.) in
-  badd_fmt s "#%02X%02X%02X" r g b
-
-let badd_dashes s = function
+  b_fmt s "#%02X%02X%02X" r g b
+    
+let b_dashes s = function
 | None -> () 
 | Some (offset, dashes) -> 
     let rec array = function
-    | [] -> badd_fmt s "\""
+    | [] -> b_fmt s "\""
     | d :: ds -> 
-        if ds = [] then badd_fmt s "%g\"" d else (badd_fmt s "%g," d; array ds)
+        if ds = [] then b_fmt s "%g\"" d else (b_fmt s "%g," d; array ds)
     in
-    if offset <> 0. then badd_fmt s " stroke-dashoffset=\"%g\"" offset;
-    badd_fmt s " stroke-dasharray=\""; 
+    if offset <> 0. then b_fmt s " stroke-dashoffset=\"%g\"" offset;
+    b_fmt s " stroke-dasharray=\""; 
     array dashes
-
-let badd_outline s o = 
+      
+let b_outline s o = 
   let w = o.P.width in 
   let c = o.P.cap in 
   let j = o.P.join in
   let ma = o.P.miter_angle in
-  if w <> P.o.P.width then badd_fmt s " stroke-width=\"%g\"" w; 
-  if c <> P.o.P.cap then badd_fmt s " stroke-linecap=\"%s\"" (cap_str c); 
-  if j <> P.o.P.join then badd_fmt s " stroke-linejoin=\"%s\"" (join_str j);
+  if w <> P.o.P.width then b_fmt s " stroke-width=\"%g\"" w; 
+  if c <> P.o.P.cap then b_fmt s " stroke-linecap=\"%s\"" (cap_str c); 
+  if j <> P.o.P.join then b_fmt s " stroke-linejoin=\"%s\"" (join_str j);
   if ma <> P.o.P.miter_angle then 
-    badd_fmt s " stroke-miterlimit=\"%g\"" (Vgr.Private.P.miter_limit o);
-  badd_dashes s o.P.dashes
-  
-let badd_svg_prim s op = function
-| Color (c, "") -> badd_fmt s " %s=\"%s\"" op c
-| Color (c, a) -> badd_fmt s " %s=\"%s\" %s-opacity=\"%s\"" op c op a
-| Gradient id -> badd_fmt s " %s=\"url(#i%d)\"" op id
-
-let badd_stop s (t, c) =
+    b_fmt s " stroke-miterlimit=\"%g\"" (Vgr.Private.P.miter_limit o);
+  b_dashes s o.P.dashes
+    
+let b_svg_prim s op = function
+| Color (c, "") -> b_fmt s " %s=\"%s\"" op c
+| Color (c, a) -> b_fmt s " %s=\"%s\" %s-opacity=\"%s\"" op c op a
+| Gradient id -> b_fmt s " %s=\"url(#i%d)\"" op id
+                   
+let b_stop s (t, c) =
   let alpha = Color.a c in
-  badd_fmt s "<stop offset=\"%g\" stop-color=\"" t;
-  badd_rgb_color s c;
-  if alpha = 1.0 then badd_fmt s "\"/>" else 
-  badd_fmt s "\" stop-opacity=\"%g\"/>" alpha
-
-let badd_gradient_transforms s trs = 
+  b_fmt s "<stop offset=\"%g\" stop-color=\"" t;
+  b_rgb_color s c;
+  if alpha = 1.0 then b_fmt s "\"/>" else 
+  b_fmt s "\" stop-opacity=\"%g\"/>" alpha
+    
+let b_gradient_transforms s trs = 
   let rec loop = function
-  | [] -> badd_fmt s "\">"
-  | t :: ts -> ignore (badd_transform s t); loop ts 
+  | [] -> b_fmt s "\">"
+  | t :: ts -> ignore (b_transform s t); loop ts 
   in
-  if trs = [] then (badd_fmt s ">") else 
-  (badd_fmt s " gradientTransform=\""; loop trs)
-
+  if trs = [] then (b_fmt s ">") else 
+  (b_fmt s " gradientTransform=\""; loop trs)
+  
 let w_primitive s p k r = try k (Hashtbl.find s.prims p) r with 
 | Not_found ->
     let rec create = function 
     | Const c, _ -> 
         let get () = let c = Buffer.contents s.buf in Buffer.clear s.buf; c in
         let a = Color.a c in
-        let cstr = (badd_rgb_color s c; get ()) in
-        let astr = if a = 1.0 then "" else (badd_fmt s "%g" a; get ()) in 
+        let cstr = (b_rgb_color s c; get ()) in
+        let astr = if a = 1.0 then "" else (b_fmt s "%g" a; get ()) in 
         Color (cstr, astr)
     | Axial (stops, p1, p2), trs ->
         let id = new_id s in
-        badd_fmt s "<defs><linearGradient gradientUnits=\"userSpaceOnUse\" \
-                    id=\"i%d\" x1=\"%g\" y1=\"%g\" x2=\"%g\" y2=\"%g\"" 
+        b_fmt s "<defs><linearGradient gradientUnits=\"userSpaceOnUse\" \
+                 id=\"i%d\" x1=\"%g\" y1=\"%g\" x2=\"%g\" y2=\"%g\"" 
           id (V2.x p1) (V2.y p1) (V2.x p2) (V2.y p2); 
-        badd_gradient_transforms s trs; 
-        List.iter (badd_stop s) stops; 
-        badd_fmt s "</linearGradient></defs>";
+        b_gradient_transforms s trs; 
+        List.iter (b_stop s) stops; 
+        b_fmt s "</linearGradient></defs>";
         Gradient id
     | Radial (stops, f, c, r), trs ->
         let id = new_id s in 
-        badd_fmt s "<defs><radialGradient gradientUnits=\"userSpaceOnUse\" \
+        b_fmt s "<defs><radialGradient gradientUnits=\"userSpaceOnUse\" \
                     id=\"i%d\" fx=\"%g\" fy=\"%g\" cx=\"%g\" cy=\"%g\" \
                     r=\"%g\""
           id (V2.x f) (V2.y f) (V2.x c) (V2.y c) r;
-        badd_gradient_transforms s trs;
-        List.iter (badd_stop s) stops; 
-        badd_fmt s "</radialGradient></defs>";
+        b_gradient_transforms s trs;
+        List.iter (b_stop s) stops; 
+        b_fmt s "</radialGradient></defs>";
         Gradient id
     | Raster _, _ -> assert false 
     in
     let svg_prim = create p in 
     Hashtbl.add s.prims p svg_prim; w_buf s (k svg_prim) r
-
+      
 let w_primitive_cut s a path_id k svg_prim r = match a with 
 | `O o ->
-    badd_fmt s "<use l:href=\"#i%d\"" path_id;
-    badd_outline s o;
-    badd_svg_prim s "stroke" svg_prim;
-    badd_str s "/>";
+    b_fmt s "<use l:href=\"#i%d\"" path_id;
+    b_outline s o;
+    b_svg_prim s "stroke" svg_prim;
+    b_str s "/>";
     w_buf s k r
 | `Anz | `Aeo ->
     let rule = if a = `Anz then "" else " fill-rule=\"evenodd\"" in 
-    badd_fmt s "<use l:href=\"#i%d\"%s" path_id rule; 
-    badd_svg_prim s "fill" svg_prim;
-    badd_str s "/>";
+    b_fmt s "<use l:href=\"#i%d\"%s" path_id rule; 
+    b_svg_prim s "fill" svg_prim;
+    b_str s "/>";
     w_buf s k r
-
+      
 let w_clip_path s a i path_id k r = 
   let clip = path_id, a in
   try k (Hashtbl.find s.clips clip) r with
@@ -285,18 +275,18 @@ let w_clip_path s a i path_id k r =
       | `Anz | `Aeo as a -> area_str a
       in
       Hashtbl.add s.clips clip id;
-      badd_fmt s "<defs><clipPath id=\"i%d\" clip-rule=\"%s\">\
-                  <use l:href=\"#i%d\"/></clipPath></defs>" id astr path_id;
+      b_fmt s "<defs><clipPath id=\"i%d\" clip-rule=\"%s\">\
+               <use l:href=\"#i%d\"/></clipPath></defs>" id astr path_id;
       w_buf s (k id) r
-
+        
 let w_clip s a i path_id k r = 
   let w_clip k clip_id r = 
     s.todo <- (Draw i) :: (pop_gstate s) :: s.todo;
-    badd_fmt s "<g clip-path=\"url(#i%d)\">" clip_id;
+    b_fmt s "<g clip-path=\"url(#i%d)\">" clip_id;
     w_buf s k r
   in
   w_clip_path s a i path_id (w_clip k) r
-
+    
 let tr_primitive i =
   let rec loop acc = function
   | Primitive (Raster _) | Blend _ | Cut _ | Cut_glyphs _ -> None
@@ -304,7 +294,7 @@ let tr_primitive i =
   | Tr (tr, i) -> loop (tr :: acc) i
   in
   loop [] i 
-
+    
 let rec w_cut s a i k path_id r = match i with 
 | Primitive (Raster _) -> 
     begin match a with 
@@ -320,22 +310,22 @@ let rec w_cut s a i k path_id r = match i with
     | Some p_trs -> w_primitive s p_trs (w_primitive_cut s a path_id k) r
     end
 | Blend _ | Cut _ | Cut_glyphs _ as i -> w_clip s a i path_id k r
-
+                                           
 let rec w_transforms s acc i k r =    (* collapses nested Tr in single <g>. *)
   if s.cost > limit s then (s.cost <- 0; partial (w_transforms s acc i k) r)else
   begin
     s.cost <- s.cost + 1;
     match i with
     | Tr (tr, i') -> 
-        let tr = badd_transform s tr in
+        let tr = b_transform s tr in
         w_buf s (w_transforms s (M3.mul acc tr) i' k) r
     | i ->
-        badd_str s "\">"; 
+        b_str s "\">"; 
         s.todo <- (Draw i) :: pop_gstate s :: s.todo;
         s.s_tr <- M3.mul s.s_tr acc;
         w_buf s k r
   end
-
+  
 let rec w_cut_glyphs s a run i k r = match i with 
 | Primitive (Raster _) | Tr _ | Blend _ | Cut _ | Cut_glyphs _ as i -> 
     warn s (`Unsupported_glyph_cut (a, image i)); k r
@@ -346,18 +336,18 @@ let rec w_cut_glyphs s a run i k r = match i with
         let font = bget_font s run.font in
         w_primitive s (p, []) begin fun svg_prim r -> 
           (* font attribute doesn't seem to work !? *)
-          badd_fmt s "<text transform=\"scale(1,-1)\" %s " font;
+          b_fmt s "<text transform=\"scale(1,-1)\" %s " font;
           begin match a with 
-          | `O o -> badd_outline s o; badd_svg_prim s "stroke" svg_prim
-          | `Anz | `Aeo -> badd_svg_prim s "fill" svg_prim
+          | `O o -> b_outline s o; b_svg_prim s "stroke" svg_prim
+          | `Anz | `Aeo -> b_svg_prim s "fill" svg_prim
           end;
-          badd_str s ">";
-          badd_esc_str s text; 
-          badd_str s "</text>"; 
+          b_str s ">";
+          b_esc_str s text; 
+          b_str s "</text>"; 
           w_buf s k r
         end r
     end
-
+    
 let rec w_image s k r =
   if s.cost > limit s then (s.cost <- 0; partial (w_image s k) r) else
   match s.todo with
@@ -384,19 +374,19 @@ let rec w_image s k r =
           w_image s k r
       | Tr _ as i ->
           s.todo <- todo;
-          badd_str s "<g transform=\""; 
+          b_str s "<g transform=\""; 
           w_transforms s M3.id i (w_image s k) r
             
 let render xml_decl s v k r = match v with 
 | `End -> w_str "</g></svg>" (Vgr.Private.flush k) r
 | `Image (size, view, i) ->
-    badd_svg s xml_decl size;
-    badd_xmp s;
-    badd_init s size view;
+    b_svg s xml_decl size;
+    b_xmp s;
+    b_init s size view;
     s.todo <- [Draw i];
     s.view <- view;
     w_buf s (w_image s k) r
-
+      
 let target ?(xml_decl = true) ?xmp () = 
   let target r _ = 
     false, 
