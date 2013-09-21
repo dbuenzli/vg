5c9f666bc5d7d1b877da3278bc7806661768b50b
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-09-21 10:05:02 +0200

    vecho example, implement kerning.

--- test/vecho.ml
+++ test/vecho.ml
@@ -43,6 +43,7 @@ type otf_info =
   { i_otf : string;                                      (* The font bytes. *)
     i_cmap : int Cmap.t;           (* Maps unicode scalar values to glyphs. *)
     i_advs : int Gmap.t;             (* Maps glyph to advances in em space. *) 
+    i_kern : int Gmap.t Gmap.t;    (* Maps glyph pairs to kern adjustement. *)
     i_units_per_em : int; }                      (* Number of units per em. *) 
   
 let add_adv acc g adv _ = Gmap.add g adv acc
@@ -55,7 +56,15 @@ let add_cmap acc kind (u0, u1) g =
       for u = u0 to u1 do acc := Cmap.add u g !acc done
   end;
   !acc
-      
+
+let add_ktable acc i = 
+  (if i.Otfm.kern_dir = `H && i.Otfm.kern_kind = `Kern then `Fold else `Skip), 
+  acc
+  
+let add_kpair acc g0 g1 kv =
+  let m = try Gmap.find g0 acc with Not_found -> Gmap.empty in
+  Gmap.add g0 (Gmap.add g1 kv m) acc
+  
 let font_info font = match font with
 | None -> `Ok ("Courier", None)
 | Some inf -> 
@@ -67,14 +76,20 @@ let font_info font = match font with
         | `Ok v -> f v 
         in
         let d = Otfm.decoder (`String i_otf) in
-        Otfm.postscript_name d          >>= fun name ->
-        Otfm.head d                     >>= fun head -> 
-        Otfm.cmap d add_cmap Cmap.empty >>= fun (_, i_cmap) ->
-        Otfm.hmtx d add_adv Gmap.empty  >>= fun i_advs ->
+        Otfm.postscript_name d                      >>= fun name ->
+        Otfm.head d                                 >>= fun head -> 
+        Otfm.cmap d add_cmap Cmap.empty             >>= fun (_, i_cmap) ->
+        Otfm.hmtx d add_adv Gmap.empty              >>= fun i_advs ->
+        Otfm.kern d add_ktable add_kpair Gmap.empty >>= fun i_kern ->
         let name = match name with None -> "Unknown" | Some n -> n in
         let i_units_per_em = head.Otfm.head_units_per_em in
-        `Ok (name, Some { i_otf; i_cmap; i_advs; i_units_per_em })
+        `Ok (name, Some { i_otf; i_cmap; i_advs; i_kern; i_units_per_em })
           
+let get_glyph fi g = try Gmap.find g fi.i_cmap with Not_found -> 0
+let get_adv fi g = try Gmap.find g fi.i_advs with Not_found -> 0
+let get_kern fi g g' = 
+  try Gmap.find g' (Gmap.find g fi.i_kern) with Not_found -> 0 
+
 (* Text layout *) 
 
 let fixed_layout size text =
@@ -85,26 +100,47 @@ let fixed_layout size text =
   let len = size *. (float (Uutf.String.fold_utf_8 add_adv 0 text)) in
   [], [], (len /. units_per_em)
 
-let otf_layout info size text =
-  let get_adv g = try Gmap.find g info.i_advs with Not_found -> 0 in
-  let get_glyph u = try Cmap.find u info.i_cmap with Not_found -> 0 in
-  let u_to_em = float info.i_units_per_em in
+let otf_layout fi size text =
+  let u_to_em = float fi.i_units_per_em in
   let rec add_glyph (gs, advs, len as acc) i = function 
   | `Malformed _ -> add_glyph acc i (`Uchar Uutf.u_rep)
   | `Uchar u ->
-      let g = get_glyph u in
-      let v_adv = get_adv g in
-      let adv = V2.v ((size *. (float v_adv)) /. u_to_em) 0. in
-      (g :: gs, adv :: advs, len + v_adv)
+      let g = get_glyph fi u in
+      let adv = get_adv fi g in
+      let sadv = V2.v ((size *. (float adv)) /. u_to_em) 0. in
+      (g :: gs, sadv :: advs, len + adv)
   in
   let gs, advs, len = Uutf.String.fold_utf_8 add_glyph ([], [], 0) text in 
   gs, advs, ((size *. (float len)) /. u_to_em)
   
+let otf_kern_layout fi size text =
+  let u_to_em = float fi.i_units_per_em in
+  let rec add (prev, gs, advs, kerns as acc) i = function 
+  | `Malformed _ -> add acc i (`Uchar Uutf.u_rep)
+  | `Uchar u ->
+      let g = get_glyph fi u in 
+      let advs = get_adv fi g :: advs in
+      let kerns = if prev = -1 then kerns else (get_kern fi prev g) :: kerns in 
+      (g, g :: gs, advs, kerns)
+  in
+  let rec advances acc len advs kerns = match advs, kerns with
+  | adv :: advs, k :: kerns -> 
+      let adv = adv + k in
+      let sadv = V2.v ((size *. (float adv)) /. u_to_em) 0. in
+      advances (sadv :: acc) (len + adv) advs kerns
+  | adv :: [], [] -> acc, len + adv
+  | _ -> assert false
+  in
+  let _, gs, advs, kerns = Uutf.String.fold_utf_8 add (-1, [], [], []) text in
+  let advs, len = advances [] 0 (List.rev advs) (List.rev kerns) in
+  gs, advs, ((size *. float len) /. u_to_em)
+
 (* Text rendering *)
 
-let renderable (fname, info) size text = 
+let renderable (fname, info) size kern text = 
   let glyphs_rev, advances_rev, len = match info with 
   | None -> fixed_layout size text
+  | Some info when kern -> otf_kern_layout info size text
   | Some info -> otf_layout info size text
   in
   let glyphs, advances = List.rev glyphs_rev, List.rev advances_rev in
@@ -125,13 +161,13 @@ let font_resolver = function
     | `Error _ as e -> e 
     | `Otf _ as otf -> `Ok (fun _ -> otf)
         
-let echo font size msg = match font_info font with
+let echo font size kern msg = match font_info font with
 | `Error _ as e -> e
 | `Ok font_info ->
     match font_resolver font_info with 
     | `Error e -> `Error (otfm_err_str e)
     | `Ok font -> 
-        let renderable = renderable font_info size msg in
+        let renderable = renderable font_info size kern msg in
         let r = Vgr.create (Vgr_pdf.target ~font ()) (`Channel stdout) in
         ignore (Vgr.render r renderable); 
         ignore (Vgr.render r `End); 
@@ -148,11 +184,13 @@ let main () =
   in
   let font = ref "" in
   let size = ref 20. in
+  let kern = ref false in
   let msg = Buffer.create 255 in
   let add_string s = Buffer.add_string msg s; Buffer.add_char msg ' ' in
   let options = [ 
     "-f", (Arg.Set_string font), " FILE, specify the OpenType font file to use";
-    "-s", (Arg.Set_float size), " SIZE, specify the font size (in mm)"
+    "-s", (Arg.Set_float size), " SIZE, specify the font size (in mm)";
+    "-k", (Arg.Set kern), " use OpenType kern table"; 
   ]
   in
   Arg.parse (Arg.align options) add_string usage;
@@ -161,7 +199,7 @@ let main () =
     Buffer.sub msg 0 (if l > 0 then l - 1 else 0) (* rem. last ' ' *) 
   in
   let font = match !font with "" -> None | f -> Some f in
-  match echo font !size msg with 
+  match echo font !size !kern msg with 
   | `Error e -> Format.eprintf "%s: %s@." exec e; exit 1 
   | `Ok  -> exit 0
   
