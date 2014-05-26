ac1229dee3d85d3c88bf26a516963bbb13f55e91
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-05-26 13:46:58 +0200

    topkg support.

---
+++ pkg/build.ml
@@ -0,0 +1,25 @@
+#!/usr/bin/env ocaml 
+#directory "pkg";;
+#use "topkg.ml";;
+
+let uutf = Env.bool "uutf"
+let otfm = Env.bool "otfm"
+let jsoo = Env.bool "jsoo"
+let vgr_pdf = uutf && otfm
+
+let () = Pkg.describe "vg" ~builder:`OCamlbuild [
+    Pkg.lib "pkg/META";
+    Pkg.lib ~exts:Exts.module_library "src/vg";
+    Pkg.lib ~exts:Exts.module_library "src/vgr_svg";
+    Pkg.lib ~cond:vgr_pdf ~exts:Exts.module_library "src/vgr_pdf";
+    Pkg.bin ~cond:vgr_pdf ~auto:true "test/vecho";
+    Pkg.lib ~cond:jsoo ~exts:Exts.module_library "src/vgr_htmlc";
+    Pkg.doc "README.md";
+    Pkg.doc "CHANGES.md";
+    Pkg.doc "test/min_htmlc.html";
+    Pkg.doc "test/min_htmlc.ml";
+    Pkg.doc "test/min_pdf.ml";
+    Pkg.doc "test/min_svg.ml"; ]
+
+
+
