501d6f723f3e822d7c720ef7f93ef0ae6de9e8d4
Arthur Wendling <art.wendling@gmail.com>
2014-12-09 11:34:54 +0100

    Add a minimal cairo2 example

---
+++ test/min_cairo2.ml
@@ -0,0 +1,29 @@
+(* This code is in the public domain.
+
+   Minimal Vgr_cairo2 example. Compile with:
+
+   ocamlfind ocamlc \
+    -package cairo2 \
+    -package gg,vg,vg.cairo2 \
+    -linkpkg -o min_cairo2.byte min_cairo2.ml
+*)
+
+open Gg
+open Vg
+
+(* 1. Define your image *)
+
+let aspect = 1.618
+let size = Size2.v (aspect *. 100.) 100. (* mm *)
+let view = Box2.v P2.o (Size2.v aspect 1.)
+let image = I.const (Color.v_srgb 0.314 0.784 0.471)
+
+(* 2. Render *)
+
+let () =
+  let surface = Cairo.Image.create Cairo.Image.ARGB32 100 100 in
+  let warn w = Vgr.pp_warning Format.err_formatter w in
+  let r = Vgr.create ~warn (Vgr_cairo2.target surface) `Other in
+  ignore (Vgr.render r (`Image (size, view, image)));
+  ignore (Vgr.render r `End);
+  Cairo.PNG.write surface "min_cairo2.png"
